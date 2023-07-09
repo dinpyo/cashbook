085deeac563f425dc2c34d3caecb462349a1bbc8
dinpyo <dinpyo@naver.com>
2023-07-09 23:09:20 +0900

    접속자수 관련 코드 추가

--- src/main/java/cash/controller/CashbookController.java
+++ src/main/java/cash/controller/CashbookController.java
@@ -8,9 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import cash.service.CounterService;
+
 @WebServlet("/cashbook")
 public class CashbookController extends HttpServlet {
-
+	private CounterService counterService = null;
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// session 인증 검사 코드
@@ -20,7 +23,16 @@ public class CashbookController extends HttpServlet {
 			return;
 		}
 		
-		// 이번달 달력에 가계부목록의 모델값을 셋팅
+		// 접속자 수 
+		this.counterService = new CounterService();
+		
+		int counter = counterService.getCounter();
+		int totalConter = counterService.getCounterAll();
+		
+		request.setAttribute("counter", counter);
+		request.setAttribute("totalCounter", totalConter);
+
+		
 		
 		request.getRequestDispatcher("/WEB-INF/view/cashbook.jsp").forward(request, response);
 	}
--- src/main/webapp/WEB-INF/view/cashbook.jsp
+++ src/main/webapp/WEB-INF/view/cashbook.jsp
@@ -7,6 +7,15 @@
 </head>
 <body>
 	<h1>cashbook.jsp</h1>
+	<div>
+		현재 접속자 : ${currentCounter} <!-- application.getAttribute("currentCounter"); -->
+	</div>
+	<div>
+		오늘 접속자 : ${counter} <!-- request.getAttribute("counter"); -->
+	</div>
+	<div>
+		누적 접속자 : ${totalCounter} <!-- request.getAttribute("totalCounter"); -->
+	</div>
 	<a href="${pageContext.request.contextPath}/logout">로그아웃</a>
 	<a href="${pageContext.request.contextPath}/memberOne">회원정보</a>
 	<a href="${pageContext.request.contextPath}/calendar">캘린더</a>
