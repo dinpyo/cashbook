53562330cbf38229d8b55d06a0befc016c22391a
hong <dinpyo@naver.com>
2023-08-14 10:06:08 +0900

    sql 파일 변경

--- src/main/webapp/WEB-INF/cash.sql
+++ src/main/webapp/WEB-INF/cash.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.5.21-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win64
+-- 호스트:                          3.34.33.114
+-- 서버 버전:                        10.1.48-MariaDB-0ubuntu0.18.04.1 - Ubuntu 18.04
+-- 서버 OS:                        debian-linux-gnu
 -- HeidiSQL 버전:                  12.3.0.6589
 -- --------------------------------------------------------
 
@@ -17,7 +17,7 @@
 
 -- cash 데이터베이스 구조 내보내기
 DROP DATABASE IF EXISTS `cash`;
-CREATE DATABASE IF NOT EXISTS `cash` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
+CREATE DATABASE IF NOT EXISTS `cash` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `cash`;
 
 -- 테이블 cash.cashbook 구조 내보내기
@@ -34,9 +34,21 @@ CREATE TABLE IF NOT EXISTS `cashbook` (
   PRIMARY KEY (`cashbook_no`),
   KEY `FK_cashbook_member` (`member_id`),
   CONSTRAINT `FK_cashbook_member` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 cash.cashbook:~0 rows (대략적) 내보내기
+-- 테이블 데이터 cash.cashbook:~11 rows (대략적) 내보내기
+INSERT INTO `cashbook` (`cashbook_no`, `member_id`, `category`, `cashbook_date`, `price`, `memo`, `updatedate`, `createdate`) VALUES
+	(17, 'admin', '수입', '2023-08-10', 316000, '#서울관악지청 #국비지원', '2023-08-12 17:30:15', '2023-08-12 17:30:15'),
+	(19, 'admin', '지출', '2023-08-10', 7000, '#점심 #바른식탁', '2023-08-12 17:31:29', '2023-08-12 17:31:29'),
+	(21, 'admin', '지출', '2023-07-04', 6500, '#점심 #밥뜨랑', '2023-08-12 17:39:47', '2023-08-12 17:39:47'),
+	(22, 'admin', '지출', '2023-07-05', 6500, '#점심', '2023-08-12 17:45:14', '2023-08-12 17:45:14'),
+	(23, 'admin', '수입', '2023-07-12', 500000, '#국민취업지원', '2023-08-14 09:04:02', '2023-08-14 09:04:02'),
+	(24, 'admin', '수입', '2023-07-10', 316000, '#서울관악지청', '2023-08-14 09:04:52', '2023-08-14 09:04:52'),
+	(25, 'admin', '수입', '2023-06-21', 75000, '#부천세무서 #종합소득세 #환급', '2023-08-14 09:06:20', '2023-08-14 09:06:20'),
+	(26, 'admin', '수입', '2023-06-12', 500000, '#국민취업지원', '2023-08-14 09:06:55', '2023-08-14 09:06:55'),
+	(27, 'admin', '수입', '2023-06-08', 284400, '#서울관악지청', '2023-08-14 09:07:25', '2023-08-14 09:07:25'),
+	(28, 'admin', '수입', '2023-05-15', 316000, '#서울관악지청', '2023-08-14 09:08:16', '2023-08-14 09:08:16'),
+	(29, 'admin', '지출', '2023-05-12', 500000, '#국민취업지원', '2023-08-14 09:09:04', '2023-08-14 09:09:04');
 
 -- 테이블 cash.counter 구조 내보내기
 DROP TABLE IF EXISTS `counter`;
@@ -44,9 +56,30 @@ CREATE TABLE IF NOT EXISTS `counter` (
   `counter_date` date NOT NULL,
   `counter_num` int(11) NOT NULL,
   PRIMARY KEY (`counter_date`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 cash.counter:~0 rows (대략적) 내보내기
+-- 테이블 데이터 cash.counter:~20 rows (대략적) 내보내기
+INSERT INTO `counter` (`counter_date`, `counter_num`) VALUES
+	('2023-07-09', 2),
+	('2023-07-17', 3),
+	('2023-07-20', 15),
+	('2023-07-21', 2),
+	('2023-07-22', 3),
+	('2023-07-23', 1),
+	('2023-07-25', 2),
+	('2023-07-26', 1),
+	('2023-07-27', 11),
+	('2023-07-28', 8),
+	('2023-07-29', 2),
+	('2023-07-30', 1),
+	('2023-08-02', 11),
+	('2023-08-03', 1),
+	('2023-08-07', 5),
+	('2023-08-08', 1),
+	('2023-08-09', 5),
+	('2023-08-12', 10),
+	('2023-08-13', 1),
+	('2023-08-14', 3);
 
 -- 테이블 cash.hashtag 구조 내보내기
 DROP TABLE IF EXISTS `hashtag`;
@@ -57,9 +90,26 @@ CREATE TABLE IF NOT EXISTS `hashtag` (
   `createdate` datetime NOT NULL,
   PRIMARY KEY (`cashbook_no`,`word`),
   CONSTRAINT `FK_hashtag_cashbook` FOREIGN KEY (`cashbook_no`) REFERENCES `cashbook` (`cashbook_no`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 cash.hashtag:~0 rows (대략적) 내보내기
+-- 테이블 데이터 cash.hashtag:~16 rows (대략적) 내보내기
+INSERT INTO `hashtag` (`cashbook_no`, `word`, `updatedate`, `createdate`) VALUES
+	(17, '국비지원', '2023-08-12 17:30:15', '2023-08-12 17:30:15'),
+	(17, '서울관악지청', '2023-08-12 17:30:15', '2023-08-12 17:30:15'),
+	(19, '바른식탁', '2023-08-12 17:31:29', '2023-08-12 17:31:29'),
+	(19, '점심', '2023-08-12 17:31:29', '2023-08-12 17:31:29'),
+	(21, '밥뜨랑', '2023-08-12 17:39:47', '2023-08-12 17:39:47'),
+	(21, '점심', '2023-08-12 17:39:47', '2023-08-12 17:39:47'),
+	(22, '점심', '2023-08-12 17:45:14', '2023-08-12 17:45:14'),
+	(23, '국민취업지원', '2023-08-14 09:04:02', '2023-08-14 09:04:02'),
+	(24, '서울관악지청', '2023-08-14 09:04:52', '2023-08-14 09:04:52'),
+	(25, '부천세무서', '2023-08-14 09:06:20', '2023-08-14 09:06:20'),
+	(25, '종합소득세', '2023-08-14 09:06:20', '2023-08-14 09:06:20'),
+	(25, '환급', '2023-08-14 09:06:20', '2023-08-14 09:06:20'),
+	(26, '국민취업지원', '2023-08-14 09:06:55', '2023-08-14 09:06:55'),
+	(27, '서울관악지청', '2023-08-14 09:07:25', '2023-08-14 09:07:25'),
+	(28, '서울관악지청', '2023-08-14 09:08:16', '2023-08-14 09:08:16'),
+	(29, '국민취업지원', '2023-08-14 09:09:04', '2023-08-14 09:09:04');
 
 -- 테이블 cash.member 구조 내보내기
 DROP TABLE IF EXISTS `member`;
@@ -69,9 +119,11 @@ CREATE TABLE IF NOT EXISTS `member` (
   `updatedate` datetime NOT NULL,
   `createdate` datetime NOT NULL,
   PRIMARY KEY (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 cash.member:~0 rows (대략적) 내보내기
+-- 테이블 데이터 cash.member:~1 rows (대략적) 내보내기
+INSERT INTO `member` (`member_id`, `member_pw`, `updatedate`, `createdate`) VALUES
+	('admin', '*A4B6157319038724E3560894F7F932C8886EBFCF', '2023-08-12 17:29:47', '2023-08-12 17:29:47');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
